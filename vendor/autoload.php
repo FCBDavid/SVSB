8451ded0947eb7b781b1170756eeebd319f00681
usuario <usuario@Toshiba>
2013-08-07 14:17:57 -0500

    instalacion de fosuserbundle

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd88cc5b89b8cfaf5883ba5eba4d565f8::getLoader();
+return ComposerAutoloaderInitfdf22b022afb01cbbbfe704de07ab52c::getLoader();
